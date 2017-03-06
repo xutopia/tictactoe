7d9bf13cfba9f216286dd8085e3e233c1838b672
xutopia <xuology@gmail.com>
2017-03-06 15:22:49 -0500

    rendered basic game logic in recursive function calls

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 var prompt = require('prompt');
 
-prompt.start();
-
-// testing the use of `prompt`
-prompt.get(['test'], function(err, result) {
-  if(err) {
-    return err;
-  }
-  console.log('testing the prompt from user');
-  console.log('here is the actual result:', result.test);
-})
+// prompt.start();
+//
+// // testing the use of `prompt`
+// prompt.get(['test'], function(err, result) {
+//   if(err) {
+//     return err;
+//   }
+//   console.log('testing the prompt from user');
+//   console.log('here is the actual result:', result.test);
+// })
 
 
 // below code is the actual game
@@ -24,8 +24,10 @@ var board = [
 function makeMove(positionX, positionY, mark) {
   if(board[positionY][positionX]) {
     console.error('invalid move');
+    return false;
   }
   board[positionY][positionX] = mark;
+  return true;
 }
 
 function printBoard() {
@@ -34,4 +36,38 @@ function printBoard() {
     ${board[1]}
     ${board[2]}`
   )
+}
+
+function checkWin() {
+  if(rowWin(board) || colWin(board) || diagonalWin(board)) {
+    return true;
+  }
+  return false;
+}
+
+function play(player) {
+  prompt.start();
+  console.log(`player ${player} turn: please enter a position in the form (x, y)`);
+  prompt.get([`position`], function(err, result) {
+    if(err) {
+      console.error(err);
+    }
+    if(validMove(result.position)) {
+      var positionX = result.position.slice(0, 1);
+      var positionY = result.position.slice(3);
+      var mark = player;
+      makeMove(positionX, positionY, mark);
+      printBoard();
+      if(checkWin()) {
+        console.log(`WINNER! ${player}`);
+        return;
+      }
+    }
+
+    if(player === 'X') {
+      play('O');
+    } else {
+      play('X');
+    }
+  })
 }
